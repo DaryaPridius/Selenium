d62f93a84e3e68efdbf036ea10f04145cd09560c
Pridius <darya.pridius@yandex.ru>
2024-08-09 16:20:35 +0300

    add a test

--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -2,16 +2,22 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardTest {
+    SelenideElement form = $(".form");
+
+    @BeforeEach
+    public void SetUp() {
+        open("http://localhost:9999");
+    }
+
     @Test
     void sendingFromWithValidData() {
-        open("http://localhost:9999");
-        SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Иванов Олег");
         form.$("[data-test-id=phone] input").setValue("+79884555555");
         form.$("[data-test-id=agreement]").click();
@@ -19,4 +25,12 @@ public class CardTest {
         $("[data-test-id=order-success]").shouldHave(Condition.text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
+    @Test
+    void sendingFormWithDoubleSurname() {
+        form.$("[data-test-id=name] input").setValue("Салтыков-Щедрин Михаил");
+        form.$("[data-test-id=phone] input").setValue("+79884555555");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
 }

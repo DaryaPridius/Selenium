c6591f32bfd00c37d8228e349550ad5c83d6ed0c
Pridius <darya.pridius@yandex.ru>
2024-08-09 14:37:56 +0300

    add a badge and the first test

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/k835rn79hpqov6sv?svg=true)](https://ci.appveyor.com/project/DaryaPridius/selenium)
--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -1,4 +1,22 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class CardTest {
+    @Test
+    void sendingFromWithValidData() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Иванов Олег");
+        form.$("[data-test-id=phone] input").setValue("+79884555555");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
 }
